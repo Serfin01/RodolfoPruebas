20b01d67f1b78c052f0b22dfdb6c11a3bfb93d75
Serfin01 <67019050+Serfin01@users.noreply.github.com>
2021-03-25 12:03:57 +0100

    dash

--- Assets/Scripts/Enemy/Dash/DashIA.cs
+++ Assets/Scripts/Enemy/Dash/DashIA.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DashIA : MonoBehaviour
+public class DashIA : Enemy
 {
-    public float lookRadius = 10f;
+    [Header("Player Nearby")]
+    [SerializeField] float lookRadius = 15f;
+    [SerializeField] float dashDistance = 5f;
 
+    [Header("Player References")]
     [SerializeField] GameObject player;
     [SerializeField] Transform trPlayer;
-    private float rotSpeed = 3.0f;
+
+    [Header("Speed Settings")]
+    [SerializeField] float rotSpeed = 3.0f;
+    //private float iniSpeed;
+    [SerializeField] float normalSpeed;
+    [SerializeField] float dashSpeed;
+    private float baseAcceleration;
+    private float baseSpeed;
+
+    bool canMove = true;
 
     Transform target;
     NavMeshAgent agent;
@@ -18,22 +30,68 @@ public class DashIA : MonoBehaviour
     {
         target = player.transform;
         agent = GetComponent<NavMeshAgent>();
+        baseAcceleration = GetComponent<NavMeshAgent>().acceleration;
+        baseSpeed = GetComponent<NavMeshAgent>().speed;
+        //iniSpeed = normalSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
-
-        if (distance <= lookRadius)
+        if (canMove)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
-            agent.SetDestination(target.position);
+            GetComponent<NavMeshAgent>().speed = baseSpeed;
+            GetComponent<NavMeshAgent>().acceleration = baseAcceleration;
+            if (distance <= lookRadius)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
+                agent.SetDestination(target.position);
+            }
+
+            if(distance <= dashDistance)
+            {
+                //Dash();
+                StartCoroutine("Dash");
+            }
         }
+        else
+        {
+            GetComponent<NavMeshAgent>().speed = 0;
+            GetComponent<NavMeshAgent>().acceleration = 0;
+        }
+
+        //GetComponent<NavMeshAgent>().speed = 10f;
+    }
+    /*
+    void Dash()
+    {
+        GetComponent<NavMeshAgent>().speed = 20f;
+        GetComponent<NavMeshAgent>().acceleration = 100f;
+
+        //GetComponent<NavMeshAgent>().speed = 10f;
+    }
+    */
+
+    IEnumerator Dash()
+    {
+        GetComponent<NavMeshAgent>().speed = dashSpeed;
+        GetComponent<NavMeshAgent>().acceleration = dashSpeed;
+        //_animator.SetTrigger("death");
+        yield return new WaitForSeconds(1f);
+        canMove = false;
+        GetComponent<NavMeshAgent>().speed = normalSpeed;
+        GetComponent<NavMeshAgent>().acceleration = baseAcceleration;
+        //normalSpeed = iniSpeed;
+        yield return new WaitForSeconds(1f);
+        canMove = true;
     }
 
-    void Speed()
+    IEnumerator Die()
     {
-        //GetComponent<NavMeshAgent>().speed = 10f;
+        canMove = false;
+        //_animator.SetTrigger("death");
+        yield return new WaitForSeconds(3f);
+        GameObject.Destroy(gameObject);
     }
 }
