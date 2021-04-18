bf8ed1258ea13c3449b1f74cf6a6d0c253f34d0e
victor126 <victorsabariego_bcnvj1ma1920@cev.com>
2021-04-18 15:25:50 +0200

    empezados los subtitulos

---
+++ Assets/Scripts/Subtitulos/CollectSubtitles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectSubtitles : MonoBehaviour
+{
+
+    public Animator animSubUp;
+    public Animator animSubDown;
+
+    public Dialogue dialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            animSubUp.SetTrigger("apear");
+            animSubDown.SetTrigger("Disapear");
+        }
+    }
+}
