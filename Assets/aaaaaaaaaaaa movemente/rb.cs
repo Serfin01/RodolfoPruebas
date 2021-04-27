a2c79cd5f62b3cda0b6e413fa0fc554e9617640e
Serfin01 <67019050+Serfin01@users.noreply.github.com>
2021-04-27 12:12:20 +0200

    newScene

---
+++ Assets/aaaaaaaaaaaa movemente/rb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rb : MonoBehaviour
+{
+    private Rigidbody rbody;
+    private Vector3 inputVector;
+    [SerializeField] float velocity;
+    [SerializeField] float gravity;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * velocity, gravity, Input.GetAxisRaw("Vertical") * velocity);
+    }
+
+    private void FixedUpdate()
+    {
+        rbody.velocity = inputVector;
+    }
+}
