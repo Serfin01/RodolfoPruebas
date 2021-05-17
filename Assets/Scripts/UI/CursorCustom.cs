a6bfd23c538628c35220f35cd9dd96247dcafe4a
Serfin01 <diwu_bcnvj1mb1920@cev.com>
2021-05-17 12:09:38 +0200

    cursor

---
+++ Assets/Scripts/UI/CursorCustom.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorCustom : MonoBehaviour
+{
+    public static CursorCustom instance;
+
+    [SerializeField] GameObject menuCursor;
+    [SerializeField] Texture2D gameCursor;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "Sergi")
+        {
+            Game();
+        }
+        else
+        {
+            Menu();
+        }
+    }
+    void Update()
+    {
+        Cursor.visible = false;
+        Vector2 mousePos = Input.mousePosition;
+        menuCursor.transform.position = mousePos;
+    }
+
+    void Menu()
+    {
+        //Cursor.SetCursor(menuCursor, Vector2.zero, CursorMode.Auto);
+        
+    }
+
+    void Game()
+    {
+        //Cursor.SetCursor(gameCursor, Vector2.zero, CursorMode.Auto);
+        
+    }
+}
