6aea2e001af8416fe41d1fbd0f39582feb8c379e
Serfin01 <sergilozano96@gmail.com>
2021-06-01 11:35:06 +0200

    descripcion hab

---
+++ Assets/Scripts/UI/ScriptDescription.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDescription : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite []habilities;
+    //[SerializeField] GameObject pergamino;
+    [SerializeField] Scrolls scrolls;
+
+    void Start()
+    {
+        spriteRenderer.sprite = habilities[scrolls.currentAbility];
+    }
+}
